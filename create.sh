8a3a6c3df97d1d73ffbe41e65266d1fdc4b52e5d
Josmar Dias <josmarnet@gmail.com>
2016-06-24 19:27:35 -0300

    create run.sh to help with LD_LIBRARY_PATH

--- create.sh
+++ create.sh
@@ -7,17 +7,29 @@ function log {
     echo -ne "${GOLD}$@${NC}\n"
 }
 
+PROGRAM_NAME=$(echo $1 | grep -oP "[^/]*$")
 TARBALL_FILE="standalone.tar.gz"
 TMP_DIR=$(mktemp -d)
+MAIN_DIR="$TMP_DIR/standalone"
+
+mkdir $MAIN_DIR
+mkdir $MAIN_DIR/lib
+
+echo -e "#!/usr/bin/env bash
+BASEDIR=\$(dirname \$0)
+export LD_LIBRARY_PATH=\$BASEDIR/lib:\$LD_LIBRARY_PATH
+\$BASEDIR/$PROGRAM_NAME \$@" >> $MAIN_DIR/run.sh
+
+chmod +x $MAIN_DIR/run.sh
 
 log "copying dinamyc libraries..."
 ldd $1 \
     | grep "=>" \
     | grep -oP '(\/[^ ]+)' \
-    | xargs -i -n 1 cp {} $TMP_DIR
+    | xargs -i -n 1 cp {} $MAIN_DIR/lib
 
 log "copying binary..."
-cp $1 $TMP_DIR
+cp $1 $MAIN_DIR
 
 log "creating tarball..."
 tar -zcf $TARBALL_FILE -C $TMP_DIR .
